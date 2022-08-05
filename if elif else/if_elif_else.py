3f016da82f8fda100d474d5d1de8b98e0a367c0a
Rofi <102268507+rofid0ank@users.noreply.github.com>
2022-08-06 00:25:14 +0700

    Update if_elif_else.py

--- if elif else/if_elif_else.py
+++ if elif else/if_elif_else.py
@@ -5,9 +5,18 @@ angka = 3
 if angka > 0:
   print('benar, angka 3 lebih besar dari angka 0')
 
-# if else
+# if, else
 nama = 'rofi'
 if nama == nama:
   print('benar, itu saya')
 else:
   print('salah, itu bukan saya')
+
+# if, elif, else
+nilai = 80
+if nilai > 80:
+  print('anda tidak mengikuti remedial')
+elif nilai == 80:
+  print('anda tidak mengikuti remedial')
+else:
+  print('anda harus mengikuti remedial')
