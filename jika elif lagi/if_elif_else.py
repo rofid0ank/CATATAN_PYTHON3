c7d1bee2e614399a6fb95b3529e4676aa63358e6
Rofi <102268507+rofid0ank@users.noreply.github.com>
2022-08-06 00:38:12 +0700

    Update and rename if elif else/if_elif_else.py to jika elif lagi/if_elif_else.py

--- jika elif lagi/if_elif_else.py
+++ jika elif lagi/if_elif_else.py
@@ -2,21 +2,21 @@
 
 # if
 angka = 3
-if angka > 0: # jika variabel nilai lebih besar dari 0 maka akan mengeksekusi 
+if angka > 0: # jika variabel nilainya true akan dieksekusi  
   print('benar, angka 3 lebih besar dari angka 0')
 
 # if, else
 nama = 'rofi'
-if nama == nama: # jika variabel nilainya true maka akan mengeksekusi 
+if nama == nama: # jika variabel nilainya true maka akan dieksekusi 
   print('benar, itu saya')
-else: # jika variabel nama nilainya false tidak akan dieksekusi 
+else: # jika variabel nilainya false tidak akan dieksekusi 
   print('salah, itu bukan saya')
 
 # if, elif, else
 nilai = 80
 if nilai > 80:
   print('anda tidak mengikuti remedial')
-elif nilai == 80:
+elif nilai == 80: # mengecek jika variabel nilainya true maka akan dieksekusi 
   print('anda tidak mengikuti remedial')
 else:
   print('anda harus mengikuti remedial')
