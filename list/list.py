7a4bba410dfae5b7983dce855e1f70973b750d98
Rofi <102268507+rofid0ank@users.noreply.github.com>
2022-08-05 20:41:58 +0700

    Create list.py

---
+++ list/list.py
@@ -0,0 +1,43 @@
+# list kosong
+list = []
+
+# list berisi item
+list = ['halo', 'dunia', True]
+
+# index list dari kiri ke kanan
+list = ['saya', 'dia', 'mereka']
+#           0           1           2
+
+# index list dari kanan ke kiri
+list = ['saya', 'dia', 'mereka']
+#           -3           -2           -1
+
+# metode-metode list
+
+# menambahkan item di akhir list
+list.append('dia')
+
+# menambahkan item di posisi tertentu menggunakan index
+list.insert(0, 'kami')
+
+# menambahkan lebih dari satu item
+list.extend(['aku', 'kamu'])
+
+# mencari index sebuah item
+list.index('kamu')
+
+# menghapus item menggunakan remove
+list.remove('aku')
+
+# mengurutkan item dari a - z
+list.sort()
+
+# mengurutkan item dari z - a
+list.reverse()
+
+# menghapus item menggunakan pop
+list.pop(0) # menghapus item yang berada di index 0
+
+
+
+
