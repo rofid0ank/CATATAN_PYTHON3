fc544f510a083aab2ddbf98bc5a12cbb56ccd4e2
Rofi <102268507+rofid0ank@users.noreply.github.com>
2022-08-06 00:18:51 +0700

    Update if_elif_else.py

--- if elif else/if_elif_else.py
+++ if elif else/if_elif_else.py
@@ -4,3 +4,10 @@
 angka = 3
 if angka > 0:
   print('benar, angka 3 lebih besar dari angka 0
+
+# if else
+nama = 'rofi'
+if nama == nama:
+  print('benar, itu saya')
+else:
+  print('salah, itu bukan saya')
