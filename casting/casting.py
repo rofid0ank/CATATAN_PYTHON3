b261038d34fa84e46807b94f7f2967eb9a68c3c4
Rofi <102268507+rofid0ank@users.noreply.github.com>
2022-08-05 18:40:57 +0700

    update cuy

--- casting/casting.py
+++ casting/casting.py
@@ -1,14 +1,21 @@
-# integer
+# casting data
+
+# data integer
 x = int(1) 
 y = int(2)
 z = int(3)
 
-# integer ke float
+# data integer ke data float
 x = float(1)
 y = float(2)
 z = float(3)
 
-# float ke string
+# data float ke data string
 x = str(1)
 y = str(2)
 z = str(3)
+
+# akan menghasilkan output string
+print(type(x))
+print(typr(y))
+print(typr(z))
