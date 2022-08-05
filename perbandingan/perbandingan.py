94e49c841894df054ba75b994147aafbbcccf555
Rofi <102268507+rofid0ank@users.noreply.github.com>
2022-08-05 23:24:14 +0700

    Create perbandingan.py

---
+++ perbandingan/perbandingan.py
@@ -0,0 +1,19 @@
+# perbandingan
+
+# lebih besar dari '>'
+a = 2 > 1
+
+# kurang dari '<'
+b = 1 < 2
+
+# lebih besar sama dengan '>='
+c = 20 >= 10
+
+# kurang dari sama dengan '<='
+d = 10 <= 20
+
+# sama dengan '=='
+e = 2 == 2
+
+# tidak sama dengan '!='
+f = 1 != 2
