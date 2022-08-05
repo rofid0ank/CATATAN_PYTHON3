66e9875b0d6515376f4b1ef32fe8644d74cd4a74
Rofi <102268507+rofid0ank@users.noreply.github.com>
2022-08-06 00:50:40 +0700

    Create input.py

---
+++ input/input.py
@@ -0,0 +1,13 @@
+# input
+
+# input string
+data = str(input('masukan data string : '))
+
+# input integer
+data = int(input('masukan data integer : '))
+
+# input float 
+data = float(input('masukan data float : '))
+
+# inputan string juga bisa menggunakan sebagi berikut
+data = input('masukan data string : ')
