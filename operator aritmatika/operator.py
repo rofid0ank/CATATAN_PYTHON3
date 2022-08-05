5cf83c8fd9d15e5069bfcca932797309196fbae4
Rofi <102268507+rofid0ank@users.noreply.github.com>
2022-08-05 23:09:06 +0700

    Create operator.py

---
+++ operator aritmatika/operator.py
@@ -0,0 +1,21 @@
+# operator aritmatika
+
+# penjumlahan '+'
+a = 1 + 4
+
+# pengurangan '-'
+b = 2 - 1
+
+# pembagian '/'
+c = 20 / 5
+
+# perkalian
+d = 10 * 2
+
+# pangkatan '**'
+e = 5 ** 2
+
+# pembagian bulat
+f = 10 // 3
+
+
