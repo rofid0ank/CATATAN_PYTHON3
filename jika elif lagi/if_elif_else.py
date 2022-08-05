5ff5f0388dc86820b7de019ca7469f3ec2d91afc
Rofi <102268507+rofid0ank@users.noreply.github.com>
2022-08-06 00:41:39 +0700

    Update if_elif_else.py

--- jika elif lagi/if_elif_else.py
+++ jika elif lagi/if_elif_else.py
@@ -1,9 +1,7 @@
 # if, elif, else
 
-# if
-angka = 3
-if angka > 0: # jika variabel nilainya true akan dieksekusi  
-  print('benar, angka 3 lebih besar dari angka 0')
+# if, else
+# Pengambilan keputusan (kondisi if else) tidak hanya digunakan untuk menentukan tindakan apa yang akan diambil sesuai dengan kondisi, tetapi juga digunakan untuk menentukan tindakan apa yang akan diambil/dijalankan jika kondisi tidak sesuai.Pada python ada beberapa statement/kondisi diantaranya adalah if, else dan elif Kondisi if digunakan untuk mengeksekusi kode jika kondisi bernilai benar.Kondisi if else adalah kondisi dimana jika pernyataan benar True maka kode dalam if akan dieksekusi, tetapi jika bernilai salah False maka akan mengeksekusi kode di dalam else.
 
 # if, else
 nama = 'rofi'
