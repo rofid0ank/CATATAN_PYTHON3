d8190a8b8330c3c4202d23f883152e3141872dcf
Rofi <102268507+rofid0ank@users.noreply.github.com>
2022-08-05 23:50:26 +0700

    Create variabel.py

---
+++ variabel/variabel.py
@@ -0,0 +1,27 @@
+# variabel
+
+# contoh variabel
+variabel_nama = 'roy'
+variabel_usia = 22
+
+# variabel tidak boleh diawali dengan angka 
+14juta = 14000000
+
+# yang benar
+juta14 = 14000000
+
+# variabel tidak boleh menggunakan kata kunci yang ada di python contoh 'if', 'while', 'for' dan sebagainya
+while = 'ucup ngulang 10 kali'
+
+# menghapus variabel menggunakan 'del'
+var = 'halo dunia'
+del(var)
+
+# mengubah isi variabel
+angka = 10
+angka = 5
+
+
+
+
+
