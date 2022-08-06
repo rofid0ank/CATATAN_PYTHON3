239d6d5d627db5feb2fde2108d6473577194bed7
Rofi <102268507+rofid0ank@users.noreply.github.com>
2022-08-06 12:37:48 +0700

    Update input.py

--- input/input.py
+++ input/input.py
@@ -9,5 +9,5 @@ data = int(input('masukan data integer : '))
 # input float 
 data = float(input('masukan data float : '))
 
-# inputan string juga bisa menggunakan sebagi berikut
+# inputan string juga bisa menggunakan sebagai berikut
 data = input('masukan data string : ')
